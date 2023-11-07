48b8e67c19c943c01268ae14b618b40c6ecc0048
gabriel <gabriellennon7@gmail.com>
2023-11-07 13:36:04 -0300

    initial section studies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
-import { Button } from "@/components/ui/button";
+"use client"
+import Image from "next/image";
+import CruzImg from '../assets/img/cruz.png'
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { StudyCard } from "@/components/studyCard";
 
 export default function Home() {
+  const [studies, setStudies] = useState([0,1])
+
   return (
-    <div className="flex items-center justify-center">
-      <Button>Teste</Button>
-    </div>
+    <main className="min-h-screen bg-[#F5F8FA]">
+      <div className="mt-12 bg-[#9CD9F8]/[0.4] rounded-s-[65px] max-w-[95%] md:max-w-[70%]" dir="rtl">
+        <div className="flex items-center flex-row-reverse justify-between p-2">
+          <h1 className="text-left text-[#3D3D45] font-medium text-lg">
+            Descubra o que Deus quer te dizer sobre diversos temas através da bíblia
+          </h1>
+          <Image
+            src={CruzImg}
+            width={120}
+            height={125}
+            alt="Imagem de uma cruz e um céu azul"
+            className="rounded-full md:max-w-[200px] md:max-h-[205px]"
+          />
+        </div>
+      </div>
+      <section className="mt-6 p-4">
+        <h2 className="text-[#3D3D45] text-lg font-medium">
+          Estude o que Ele diz através da bíblia
+        </h2>
+        <Input 
+          className="border-[#DCE2E5] rounded-full mt-3 h-11"
+          placeholder="Busque por um assunto de interesse"
+        />
+
+        <div className="mt-6">
+          <h2 className="text-[#3D3D45] text-xl font-bold">
+            Categorias
+          </h2>
+          <div className="grid grid-cols-2 gap-3 mt-2">
+            {studies.map(study => (
+              <StudyCard key={study} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
---
+++ src/components/studyCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import { Card, CardContent, CardFooter } from "./ui/card"
+import { Toggle } from "./ui/toggle"
+import JesusQuadriculadoImg from '../assets/img/estudos/jesus.jpeg'
+
+export const StudyCard = () => {
+  return (
+    <Card>
+      <CardContent 
+        className="p-0"
+      >
+        <Image
+          src={JesusQuadriculadoImg}
+          alt="Imagem"
+          className="w-full h-28 object-cover rounded-tl-[14px] rounded-tr-[14px]"
+        />
+      </CardContent>
+      <CardFooter className="p-[8px]">
+        <div className="flex flex-col gap-2">
+          <p>
+            Como ser perdoado?
+          </p>
+          <div className="flex flex-row gap-[4px]">
+            <Toggle variant="outline" className="bg-[#D9D9D9] text-[#50555A]">
+              <span className="font-medium text-xs">
+                perdão
+              </span>
+            </Toggle>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
